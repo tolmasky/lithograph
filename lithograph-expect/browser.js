c30464f9e9bcdb7b3664563e6c61ea633d596316
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2018-06-18 14:16:01 -0700

    Bump versions.
    Reviewed by @tolmasky.

--- lithograph-expect/browser.js
+++ lithograph-expect/browser.js
@@ -3,20 +3,30 @@ const mock = require("jest-mock");
 const jestExpect = require("expect");
 const delay = timeout => new Promise(resolve => setTimeout(resolve, timeout));
 
-module.exports = function expect(...args)
-{
-    const expectation = jestExpect(...args);
+
+module.exports = function expect(expected)
+{/*
+    Object.assign(jestExpect(expected),
+    {
+        
+    });*/
+    const expectation = jestExpect(expected);
 
     expectation.eventually = Object
         .keys(expectation)
         .reduce((matchers, key) =>
-            (matchers[key] = async (...args) =>
+            (matchers[key] = async (received) =>
             {
+                try { await expected; } catch (e) { }
+
                 while (true)
                 {
                     try
                     {
-                        return await expectation[key](...args);
+                        if (expected && typeof expected.rerun === "function")
+                            return await jestExpect(await expected.rerun())[key](received);
+
+                        return await expectation[key](...received);
                     }
                     catch (error) { }
 
