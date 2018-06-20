460928903eaec3850f725e5fa8d880051068f441
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2018-06-20 09:45:50 -0700

    Magic in forked-require.
    Reviewed by @tolmasky.

--- forked-require/forked-require.js
+++ forked-require/forked-require.js
@@ -1,18 +1,18 @@
-const { fork } = require("child_process");
+const { spawn } = require("child_process");
 const { Map, EmptyMap = Map(), Record } = require("immutable");
 
 const RemoteCall = Record({ resolve:0, reject:0, timeoutID: -1 });
-const RemoteFork = Record({ env:null, filename:"", process:0, calls: EmptyMap, nextCall:0 });
+const RemoteFork = Record({ options:null, filename:"", process:0, calls: EmptyMap, nextCall:0 });
 let RemoteRequire = Record({ forks: EmptyMap, exited: false, exports: EmptyMap })();
 
 
-module.exports = function (filename, env)
+module.exports = function (filename, options)
 {
-    const UUID = JSON.stringify({ filename, env });
+    const UUID = JSON.stringify({ filename, options });
 
     if (!RemoteRequire.exports.has(UUID))
         RemoteRequire = RemoteRequire
-            .setIn(["forks", UUID], initRemoteFork(UUID, filename, env))
+            .setIn(["forks", UUID], initRemoteFork(UUID, filename, options))
             .setIn(["exports", UUID], (...args) =>
                 new Promise(function (resolve, reject)
                 {
@@ -41,7 +41,7 @@ process.on("exit", function ()
     RemoteRequire.forks.forEach(({ process }) => process.kill("SIGHUP"));
 });
 
-function initRemoteFork(UUID, filename, env)
+function initRemoteFork(UUID, filename, options)
 {
     const remoteForkExited = () => exited(UUID);
     const listeners = 
@@ -55,13 +55,12 @@ function initRemoteFork(UUID, filename, env)
 
     const forked = Object.keys(listeners).reduce(
         (forked, event) => forked.on(event, listeners[event]),
-        fork(`${__dirname}/forked-wrapper.js`, [filename],
-            Object.assign(process.env, env)));
-
+        fork(filename, options));
+console.log(forked);
     forked.unref();
-    forked.channel.unref();
+//    forked.channel.unref();
 
-    return RemoteFork({ env, filename, process: forked, listeners });
+    return RemoteFork({ options, filename, process: forked, listeners });
 }
 
 function exited(UUID)
@@ -110,5 +109,18 @@ function forwardRemoteCall(UUID, { identifier, result })
 
     RemoteRequire = RemoteRequire
         .removeIn(["forks", UUID, "calls", identifier]);
+}
+
+function fork(filename, options)
+{
+    const env = Object.assign(process.env, options.env);
+    const args = [`${__dirname}/forked-wrapper.js`, filename];
+    const spawnOptions = { env, stdio:[0, 1, 2] };
+
+    if (options.magicWS)
+        return spawn("node_modules/magic-ws/bin/magic-ws",
+            [options.magicWS, "node", ...args], spawnOptions);
+
+    return spawn("node", args, spawnOptions);
 }
 
