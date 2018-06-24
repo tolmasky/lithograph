a1aae488411cfb2bbd9b5a50d027757397ba0353
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2018-06-24 10:08:30 -0700

    Support yarn.
    Reviewed by @tolmasky.

--- lithograph/test-worker/test-worker.js
+++ lithograph/test-worker/test-worker.js
@@ -4,8 +4,11 @@ const launched = new Promise((resolve, reject) =>
         require("puppeteer").launch({ headless })
             .then(resolve, reject)));
 
+const { dirname } = require("path");
+const puppeteerPath = dirname(require.resolve("puppeteer"));
+
 const getPackageDescriptions = require("magic-ws/get-package-descriptions");
-const packageDescriptions = getPackageDescriptions([], [`${__dirname}/../node_modules/puppeteer`]);
+const packageDescriptions = getPackageDescriptions([], [puppeteerPath]);
 
 require("magic-ws/modify-resolve-lookup-paths")(packageDescriptions);
 require("./static");
