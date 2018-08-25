30f6811fb56244977976c25326fee9ed0fc98908
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2018-08-25 14:37:56 -0700

    Beginning work on Finished.
    Reviewed by @tolmasky.

--- lithograph/test/file-process.js
+++ lithograph/test/file-process.js
@@ -1,18 +1,10 @@
 const { Cause, IO, field, event, update } = require("cause");
-const Process = require("@cause/process");
-const Parent = require("@cause/process/parent");
 const FileExecution = require("./file-execution");
-console.log("IN SEPARATE PROCESS");
+
+
 const FileProcess = Cause("FileProcess",
 {
-    [field `parent`]: Parent.create(),
     [field `fileExecution`]: -1,
-
-    [event.out `Ready`]: { },
-    [event.on (Parent.Ready)]: (fileProcess, { path }) =>
-        update.in(fileProcess,
-            "parent",
-            Parent.Message({ event: FileProcess.Ready() })),
 
     [event.in `Execute`]: { path:-1 },
     [event.on `Execute`]: (fileProcess, { path }) =>
@@ -24,14 +16,10 @@ const FileProcess = Cause("FileProcess",
 
     [event.on (Cause.Start)]: event.ignore,
 
-    [event.out `Executed`]: { },
+    [event.out `Executed`]: { path: -1 },
     [event.on (FileExecution.Finished)]: fileProcess =>
-        [fileProcess, [FileProcess.Executed()]]
+        (path => [fileProcess, [FileProcess.Executed({ path })]])
+        (fileProcess.fileExecution.path)
 });
 
-FileProcess.fork = () => Process.node({ path: __filename });
-
 module.exports = FileProcess;
-
-if (require.main === module)
-    IO.toPromise(FileProcess.create({ }));
--- lithograph/test/main.js
+++ lithograph/test/main.js
@@ -1,82 +1,63 @@
-const { Range, List } = require("immutable");
+const { Range, List, Map } = require("immutable");
 const { Cause, field, event, update, IO } = require("cause");
 const Pool = require("@cause/pool");
-const Process = require("@cause/process");
+const Fork = require("@cause/fork");
 const FileProcess = require("./file-process");
 
 
-module.exports = function main(paths, options)
+module.exports = async function main(paths, options)
 {
     const requires = JSON.stringify(options.requires || []);
+    const promise =
+        IO.toPromise(Main.create({ ...options, paths, requires }));
 
-    return IO.toPromise(Main.create({ ...options, paths, requires }));
+    await promise;
+
+    console.log("NOW DONE");
 }
 
 const Main = Cause("Main",
 {
     [field `paths`]: -1,
+    [field `reports`]: Map(),
     [field `fileProcessPool`]: -1,
     [field `fileProcesses`]: -1,
 
-    init({ paths, concurrency })
+    init({ paths: iterable, concurrency })
     {
         const fileProcessPool = Pool.create({ count: concurrency });
         const fileProcesses = Range(0, concurrency)
-            .map(index => List.of(FileProcess.fork(), false))
+            .map(() => Fork(FileProcess))
             .toList();
+        const paths = List(iterable);
 
         return { fileProcessPool, fileProcesses, paths };
-    },
-
-    [event.on (FileProcess.Ready)](main, event, fromKeyPath)
-    {
-        const index = fromKeyPath.next.data;
-        const updated = main.setIn(["fileProcesses", index, 1], true);
-        const ready = updated.fileProcesses.every(([_, ready]) => ready);
-
-        return ready ?
-            update.in(updated, "fileProcessPool",
-                Pool.Enqueue({ requests: main.paths })) :
-            updated;
     },
 
     [event.on (FileProcess.Executed)](main, event, fromKeyPath)
     {
         const index = fromKeyPath.next.data;
-        console.log("LOUD AND CLEAR FROM " + index);
-        return update.in(
-            main,
+        const reported = main.setIn(["reports", event.path], true);
+        const finished = reported.reports.size === reported.paths.size;
+        const [updated, events] = update.in(
+            reported,
             "fileProcessPool",
             Pool.Release({ indexes: [index] }));
+
+        return [updated, [...events, finished && Cause.Finished()]];
     },
 
-    [event.on (Process.Started)]: event.ignore,
-
     [event.on (Pool.Retained)]: (main, { request: path, index }) =>
-        update.in(main, ["fileProcesses", index, 0],
-            Process.Message({ event: FileProcess.Execute({ path }) })),
+        update.in(
+            main,
+            ["fileProcesses", index],
+            FileProcess.Execute({ path })),
 
     [event.on (Cause.Start)]: main =>
         update.in.reduce(main,
-            main.fileProcesses.map((_, index) =>
-                [["fileProcesses", index, 0], Cause.Start()])),
-/*
-    [event.on (Pool.Released)]: (main, { indexes })
-    
-    [event.on (Process.Message())]: (main)
-
-    [event.on (Pool.Allotted)]: (main, { allotments }) =>
-        update.in.all(alottment.map(({ index, request }) =>
-            [["fileProcess", index], Process.Message({ path: request })],
-            main),
-        
-        
-        fileExecution.set("running", fileExecution.running
-            .merge(Map(allotments.map(({ request: path, key }) =>
-                [path.data.id, IO.fromAsync(() => testRun({ path, key })) ])))),
-        request: path, index
-        update.in(main, ["fileProcesses", index], Process.Message({ path })),
-
-    [event.on (Cause.Start)]: main =>
-        update.in(main, ["fileProcessPool"], Pool.Enqueue({ requests: main.paths }))*/
+        [
+            ...main.fileProcesses.map((_, index) =>
+                [["fileProcesses", index], Cause.Start()]),
+            ["fileProcessPool", Pool.Enqueue({ requests: main.paths })]
+        ])
 });
