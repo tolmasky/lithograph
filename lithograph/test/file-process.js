e1fdfce79054a972da7f9da2290d10f45bfe5826
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2018-09-01 14:44:16 +0100

    Updates to support fixes in library and to get browser fetching closer.
    Reviewed by @tolmasky.

--- lithograph/test/file-process.js
+++ lithograph/test/file-process.js
@@ -1,29 +1,91 @@
+const { Map } = require("immutable");
 const { Cause, IO, field, event, update } = require("cause");
 const FileExecution = require("./file-execution");
 
 
 const FileProcess = Cause("FileProcess",
 {
+    // FIXME: Make this not necessary...
+    [event.on (Cause.Start)]: event.ignore,
+
+    [field `ready`]: false,
     [field `fileExecution`]: -1,
+    [field `generateGetEnvironment`]: IO.start(generateGetEnvironment),
 
     [event.in `Execute`]: { path:-1 },
     [event.on `Execute`]: (fileProcess, { path }) =>
-        update.in(
-            fileProcess.set("fileExecution",
-                FileExecution.create({ path })),
-            "fileExecution",
-            Cause.Start()),
+    {
+        const environment = fileProcess.getEnvironment();
+        const fileExecution =
+            FileExecution.create({ path, environment });
 
-    [event.on (Cause.Start)]: event.ignore,
+        return update.in(
+            fileProcess.set("fileExecution", fileExecution),
+            "fileExecution",
+            Cause.Start());
+    },
 
     [event.out `Executed`]: { path: -1 },
     [event.on (FileExecution.Finished)]: fileProcess =>
         (path => [fileProcess, [FileProcess.Executed({ path })]])
         (fileProcess.fileExecution.path),
 
-    [event.on (FileExecution.BrowserRequest)]: event.bubble,
-    [event.on (FileExecution.BrowserResponse)]: (fileProcess, event) =>
-        update.in(fileProcess, "fileExecution", event)
+    //     
+    [field `getEnvironment`]: -1,
+
+    [event.in `GeneratedGetEnvironment`]: { getEnvironment: -1 },
+    [event.on `GeneratedGetEnvironment`]: (fileProcess, { getEnvironment }) =>
+    [
+        fileProcess
+            .set("getEnvironment", getEnvironment)
+            .set("ready", true),
+        [Cause.Ready()]
+    ],
+
+    // 
+    [field `getBrowserCallbacks`]: Map({ id: 0 }),
+
+    [event.in `GetBrowserCalled`]: { resolve: -1, reject: -1 },
+    [event.on `GetBrowserCalled`](fileProcess, event)
+    {
+        const id = fileProcess.browserRequests.get("id");
+        const outFileProcess = fileProcess.set("getBrowserCallbacks",
+            fileProcess
+                .browserCallbacks
+                .concat({ id: id + 1, [id]: event }));
+
+        return [outFileProcess, FileProcess.BrowserRequest({ id })];
+    },
+
+    [event.out `EndpointRequest`]: { id: -1 },
+    [event.in `EndpointResponse`]: { id: -1, endpoint: -1 },
+    [event.on `EndpointResponse`]: (fileProcess, event) =>
+        (console.log("GOT " + event.endpoint), fileProcess)
 });
 
 module.exports = FileProcess;
+
+function generateGetEnvironment(push)
+{
+    const template = { getBrowser, getBrowserContext };
+    const getEnvironment = () => Map(Object.keys(template)
+        .map(key => [key, (...args) => template[key](...args)]))
+        .toObject();
+    const event = FileProcess.GeneratedGetEnvironment({ getEnvironment });
+
+    push(event);
+
+    async function getBrowserContext()
+    {
+        return await (await getBrowser(push))
+            .createIncognitoBrowserContext();
+    }
+
+    async function getBrowser()
+    {
+        const browserWSEndpoint = await new Promise((resolve, reject) =>
+            push(FileExecution.BrowserRequest({ resolve, reject })));
+
+        return await require("puppeteer").connect({ browserWSEndpoint });
+    }
+}
