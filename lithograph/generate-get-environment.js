1c9e16cfd304049ae8d3a6936bb7f3a39a717625
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2018-09-09 22:48:30 +0100

    Clean up and fix mock, etc.
    Reviewed by @tolmasky.

--- lithograph/generate-get-environment.js
+++ lithograph/generate-get-environment.js
@@ -1,16 +1,14 @@
 const { Map } = require("immutable");
-const mock = require("jest-mock");
-const jestExpect = require("expect");
-const fetch = require("node-fetch");
-const delay = timeout => new Promise(resolve => setTimeout(resolve, timeout));
+const testEnvironment = require("./test-worker/test-environment");
 require("./magic-ws-puppeteer");
-global.fetch = fetch;
 require("./test-worker/static");
+
+global.fetch = testEnvironment.fetch;
 
 module.exports = function generateGetEnvironment(push)
 {
     const FileProcess = require("./file-process");
-    const template = { getBrowser, getBrowserContext, mock, expect };
+    const template = { getBrowser, getBrowserContext, ...testEnvironment };
     const getEnvironment = () => Map(Object.keys(template)
         .map(key => [key, (...args) => template[key](...args)]))
         .toObject();
@@ -32,39 +30,3 @@ module.exports = function generateGetEnvironment(push)
         return await require("puppeteer").connect({ browserWSEndpoint });
     }
 }
-
-function expect(expected)
-{/*
-    Object.assign(jestExpect(expected),
-    {
-        
-    });*/
-    const expectation = jestExpect(expected);
-
-    expectation.eventually = Object
-        .keys(expectation)
-        .reduce((matchers, key) =>
-            (matchers[key] = async (received) =>
-            {
-                try { await expected; } catch (e) { }
-
-                while (true)
-                {
-                    try
-                    {
-                        if (expected && typeof expected.rerun === "function")
-                            return await jestExpect(await expected.rerun())[key](received);
-
-                        return await expectation[key](received);
-                    }
-                    catch (error) { }
-
-                    await delay(50);
-                }
-            }, matchers),
-            Object.create(null));
-
-    return expectation;
-}
-
-
