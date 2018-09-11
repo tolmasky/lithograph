c017cd34888819425a59a7792ef70a6a0fb41433
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2018-09-11 19:07:03 +0100

    Add garbage collector.
    Reviewed by @tolmasky.

---
+++ lithograph/get-frames.js
@@ -0,0 +1,22 @@
+const ErrorRegExp = /(?:(?:^Error\n\s+)|(?:\n\s+))at\s+/;
+const FrameRegExp = /\(([^\(]+):(\d+):(\d+)\)$/;
+const toInt = string => parseInt(string, 10);
+
+///^Error\n(?:\s+at[^\n]+\n){2}\s+at\s+([^\(]+\(([^\)]+)[^\n]+)/;
+const LocationRegExp = /^.+\:\d+\:\d+$/;
+
+
+module.exports = function getFrames(name)
+{
+    const { stackTraceLimit } = Error;
+    Error.stackTraceLimit = Infinity;
+    const frames = Error().stack.split(ErrorRegExp)
+        .map(frame => frame.match(FrameRegExp))
+        .filter(frame => !!frame)
+        .map(([, path, line, column]) =>
+            ({ path, line: toInt(line), column: toInt(column) }))
+        .slice(1);
+    Error.stackTraceLimit = stackTraceLimit;
+
+    return frames;
+}
