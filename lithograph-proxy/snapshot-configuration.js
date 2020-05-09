bd39ec311f1e98a46520a2fef884337b99b64461
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2020-05-09 15:44:47 -0700

    Fix for #24 Snapshots of files over ~1MB or so are cut off (on macOS).
    Reviewed by @tolmasky.

--- lithograph-proxy/snapshot-configuration.js
+++ lithograph-proxy/snapshot-configuration.js
@@ -56,8 +56,22 @@ const toTarRead = (function ()
     {
         return function tarRead(filename, format)
         {
-            const { stdout } =
-                spawn("tar", ["-xvOf", tarPath, toTarPath(filename)]);
+            const inTarPath = toTarPath(filename);
+
+            // Node limits the size of the stdout buffer in `spawn`, so we need
+            // to make sure we make it big enough for the output. I'm not a huge
+            // fan of the magic number searching here, we should probably store
+            // the size in the manifest file. Maybe we should also be storing it
+            // *as* its original gzip too?
+            const maxBuffer = +(spawn("tar", ["-vtf", tarPath])
+                .stdout.toString()
+                .split("\n")
+                .find(line => line.endsWith(inTarPath))
+                .split(/\s+/)[4]);
+
+            const { stdout } = spawn("tar",
+                ["-xvOf", tarPath, inTarPath],
+                { maxBuffer });
 
             return format === "utf-8" ?
                 stdout.toString(format) :
